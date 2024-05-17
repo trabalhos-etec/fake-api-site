90990b27f750c4acdf34866051a7c051ac2d6b9e
trabalhos-etec <165843427+trabalhos-etec@users.noreply.github.com>
2024-05-17 09:33:32 -0300

    Add files via upload

---
+++ src/index.js
@@ -0,0 +1,14 @@
+let sidebar = document.querySelector(".sidebar");
+let closeBtn = document.querySelector("#btn");
+let searchBtn = document.querySelector(".bx-search");
+
+closeBtn.addEventListener("click", ()=>{
+  sidebar.classList.toggle("open");
+  menuBtnChange();
+});
+
+searchBtn.addEventListener("click", ()=>{ 
+  sidebar.classList.toggle("open");
+  menuBtnChange(); 
+});
+
